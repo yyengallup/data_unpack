7b90e086983c429ef94f3db93375ba3548975331
YYen <yyengallup@gmail.com>
2022-07-09 13:11:20 -0700

    parameterize datapath

--- data_unpack_datapath.sv
+++ data_unpack_datapath.sv
@@ -1,39 +1,44 @@
-module data_unpack_datapath(
+module data_unpack_datapath #(
+  parameter DATA_BITWIDTH = 5,
+  parameter OUTPUT_SIZE = 7,
+
+  parameter DATA_SIZE = 2 ** DATA_BITWIDTH
+) (
   input wire clk,
   input wire rst,
 
-  input wire [31:0] data_in, //32 bit word line in
+  input wire [DATA_SIZE - 1:0] data_in, //32 bit word line in
   input wire data_rst, //reset data word buffer
   input wire data_load, //load data word buffer
   input wire data_overflow_load, //load data into overflow buffer from main data buffer
   input wire count_set, //reset counter to 6
   input wire count_en, //enable counter
 
-  output logic [6:0] data_out, //packet output
-  output logic [4:0] count //counter output for state machine transitions
+  output logic [OUTPUT_SIZE - 1:0] data_out, //packet output
+  output logic [DATA_BITWIDTH - 1:0] count //counter output for state machine transitions
 );
 
 
-  logic [-1:-6] data_overflow;
-  logic [31:0] data_buf;
+  logic [-1:1 - OUTPUT_SIZE] data_overflow;
+  logic [DATA_SIZE - 1:0] data_buf;
 
-  logic [31:-6] all_data;
+  logic [DATA_SIZE - 1:1 - OUTPUT_SIZE] all_data;
 
   assign all_data = {data_buf, data_overflow}; //for easy addressing
-  assign data_out = all_data[count-:7]; //muxes/address decoders
+  assign data_out = all_data[count-:OUTPUT_SIZE]; //muxes/address decoders
 
   //registers
   always_ff @(posedge clk) begin
     //overflow DFF with enable
-    if (rst) data_overflow <= 6'b0;
-    else     data_overflow <= data_overflow_load ? data_buf[31:26] : data_overflow;
+    if (rst) data_overflow <= 0;
+    else     data_overflow <= data_overflow_load ? data_buf[DATA_SIZE - 1:DATA_SIZE - OUTPUT_SIZE - 1] : data_overflow;
 
     //data buffer DFF with enable
-    if (rst | data_rst) data_buf <= 32'b0;
+    if (rst | data_rst) data_buf <= 0;
     else                data_buf <= data_load ? data_in : data_buf;
 
     //counter DFF with enable and count logic. count_set sets to 6
-    if (rst) count <= 5'b0;
-    else     count <= count_en ? (count_set ? 5'd6 : count + 5'd7) : count;
+    if (rst) count <= 0;
+    else     count <= count_en ? (count_set ? OUTPUT_SIZE - 1 : count + OUTPUT_SIZE) : count;
   end
 endmodule
